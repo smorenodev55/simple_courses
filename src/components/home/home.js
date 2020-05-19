fb05369277a2326bb56674ebb9b1d224cceeccb2
STEVEN MACBOOK <smoreno@softusco.com>
2020-05-19 11:57:23 -0500

    (fix): cambio de nombre

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -37,7 +37,7 @@ class Home extends Component {
     return (
       <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-warning">
-        <a class="navbar-brand" href="#">Surf Courses</a>
+        <a class="navbar-brand" href="#">Simple Courses</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
